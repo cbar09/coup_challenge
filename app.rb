29e9454c7d49fd25144cf4666c22596d6944aead
Carl Barth <carl@zappling.com>
2017-12-08 17:13:34 -0500

    added heroku config files for rack deployment

--- app.rb
+++ app.rb
@@ -1,28 +1,25 @@
 # myapp.rb
 require 'sinatra'
 require_relative 'coup_challenge'
+require 'better_errors'
+
+# Just in development!
+configure :development do
+  use BetterErrors::Middleware
+  # you need to set the application root in order to abbreviate filenames
+  # within the application:
+  BetterErrors.application_root = File.expand_path('..', __FILE__)
+end
 
 get '/' do
   NUM_DISTRICTS = (1..100)
   NUM_SCOOTERS = (0..1000)
-  c = ARGV.length >= 1 ? ARGV[0].to_i : rand(1..10)
-  p = ARGV.length >= 2 ? ARGV[1].to_i : rand(1..15)
+  c = ARGV.length >= 1 ? ARGV[0].to_i : rand(1..999)
+  p = ARGV.length >= 2 ? ARGV[1].to_i : rand(1..1000)
   n = ARGV.length >= 3 ? ARGV[2].split(',').map(&:to_i) : Array.new(rand(NUM_DISTRICTS)) { rand(NUM_SCOOTERS) }
 
-  puts "scooters[] => #{n}"
-  puts "C: #{c}"
-  puts "P: #{p}"
-
-  max_saved = max_engineers_can_be_saved(c, p)
-  min_saved = min_engineers_can_be_saved(c, p)
-  puts "The maximum number of FE's we can save is: #{max_saved}"
-  puts "The minimum number of FE's we can save is: #{min_saved}"
-
-  min_fe = solve(n, c, p)
-  total_scooters = n.reduce( :+ )
-  puts "For this configuration of scooters, we need at least #{min_fe} Field Engineers"
-  puts "Total number of scooters: #{total_scooters}"
-  puts "Total scooters divided by P = #{total_scooters / p.to_f}"
-  "For this configuration of scooters, we need at least #{min_fe} Field Engineers <br/>"
-  "Total scooters divided by P = #{total_scooters / p.to_f}"
+  @coup_challenge = CoupChallenge.new(n, c, p)
+  @min_fe = @coup_challenge.solve
+  
+  erb :show_challenge
 end
--- coup_challenge.rb
+++ coup_challenge.rb
@@ -1,59 +1,68 @@
 #!/usr/bin/env ruby
-require 'pp'
+require 'json'
 
-
-def num_fleet_engineers(n, p)
-  return (n / p.to_f).ceil
-end
-
-def num_fleet_engineers_with_fm(n, c, p)
-  return ((n - c) / p.to_f).ceil
-end
-
-def max_engineers_can_be_saved(c, p)
-  return (c / p.to_f).ceil
-end
-
-def min_engineers_can_be_saved(c, p)
-  return max_engineers_can_be_saved(c, p) - 1
-end
-
-def number_fe_saved(num_scooters, c, p)
-  num_fe_only = (num_scooters / p.to_f).ceil
-  num_fe_with_fm = ([0, (num_scooters - c)].max / p.to_f).ceil
-  puts "Number FE Saved for #{num_scooters} scooters: #{num_fe_only - num_fe_with_fm}"
-  return num_fe_only - num_fe_with_fm
-end
-
-def can_save_max?(num_scooters, c, p)
-  puts "num_scooters: #{num_scooters} | C: #{c} | P: #{p}"
-  return max_engineers_can_be_saved(c, p) == number_fe_saved(num_scooters, c, p)
-end
-
-def solve(scooters, c, p)
-  return 0 if scooters.empty?
+class CoupChallenge
   
-  solution = Hash.new(scooters.length)
+  attr_accessor :scooters 
+  attr_accessor :c 
+  attr_accessor :p
   
-  # max = false #if we find a district where we can save the max
-   max_fe_saved_theoretical = max_engineers_can_be_saved(c, p)
-   max_fe_saved = 0
-   total_FE = 0
-  
-  scooters.each_with_index do |i, index|
-    max_fe_saved = [max_fe_saved, number_fe_saved(i, c, p)].max if max_fe_saved < max_fe_saved_theoretical
-    num_fe = num_fleet_engineers(i, p) 
-    total_FE += num_fe
-    
-    solution[index] = {
-      scooters: i,
-      num_fe_only: num_fe,
-      num_fe_saved: max_fe_saved
-    }
+  def initialize(n = [], c = 0, p = 0)
+    @scooters, @c, @p = n, c, p
+    @solution = Hash.new(@scooters.length)
   end
   
-  puts "Total_FE: #{total_FE}"
-  puts "Max Saved: #{max_fe_saved}"
-  pp solution
-  return total_FE - max_fe_saved
+  def total_scooters
+    @scooters.reduce( :+ )
+  end
+  
+  def num_fleet_engineers(n)
+    return (n / @p.to_f).ceil
+  end
+  
+  def show_solution
+   JSON.pretty_generate(@solution)
+  end
+
+  def max_engineers_can_be_saved
+    return (@c / @p.to_f).ceil
+  end
+  
+  def min_engineers_can_be_saved
+    return [max_engineers_can_be_saved - 1, 0].max
+  end
+
+  def number_fe_saved(num_scooters)
+    num_fe_only = (num_scooters / @p.to_f).ceil
+    num_fe_with_fm = ([0, (num_scooters - @c)].max / @p.to_f).ceil
+    return num_fe_only - num_fe_with_fm
+  end
+
+  def can_save_max?(num_scooters)
+    return max_engineers_can_be_saved(@c, @p) == number_fe_saved(num_scooters, @c, @p)
+  end
+
+  def solve
+    return 0 if @scooters.empty?
+  
+    max_fe_saved_theoretical = max_engineers_can_be_saved
+    max_fe_saved = 0
+    total_FE = 0
+  
+    @scooters.each_with_index do |i, index|
+      max_fe_saved = [max_fe_saved, number_fe_saved(i)].max if max_fe_saved < max_fe_saved_theoretical
+      num_fe = self.num_fleet_engineers(i) 
+      total_FE += num_fe
+    
+      @solution[index] = {
+        num_scooters: i,
+        num_fe_only: num_fe,
+        num_fe_saved: max_fe_saved
+      }
+    end
+  
+    return total_FE - max_fe_saved
+  end
+
 end
+
